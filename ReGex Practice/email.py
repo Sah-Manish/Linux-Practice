2c9231d4f235b17278ef9fa30ef86bfe05286f19
Sah-Manish <sahmanish10987@gmail.com>
2024-07-01 17:51:26 +0530

    email update with flag option rather then using string method lower | re.IGNORECASE

--- ReGex Practice/email.py
+++ ReGex Practice/email.py
@@ -3,7 +3,8 @@ import re
 import sys
 
 email=sys.argv[1]
-if (re.search(r"^\w+@\w+\.(com|gov|in|net|edu)$", email)):
+if (re.search(r"^\w+@\w+\.(com|gov|in|net|edu)$", email, re.IGNORECASE)):
+    # or i could use r"\w+@\w+\.(com|gov|in|net|edu)$", email.lower()
     print("Valid")
 else:
     print("Invalid")
